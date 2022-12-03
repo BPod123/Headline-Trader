1d36cbc400db0bdacb582fd65aa574c3eb6dcccf
Ben Podrazhansky <benpodrazhansky@gmail.com>
2022-12-02 19:03:05 -0500

    Tested on multiple stocks

--- BOW_data_format.py
+++ BOW_data_format.py
@@ -1,9 +1,10 @@
 import os
-from data import load_data
+from data import load_data_memory_efficient
 import nltk
 from nltk.lm import Vocabulary
 import html
 import re
+import numpy as np
 from collections import Counter
 numbers = re.compile("[+-]?([0-9]+([.][0-9]*)?|[.][0-9]+)")
 contains_letters = re.compile('[a-zA-Z]')
@@ -47,15 +48,29 @@ def to_input_label_list(dataset):
 
 def prepare_data():
     db_path = os.path.join(os.path.split(os.getcwd())[0], 'Data\Headlines.db')
-    train, validation, test = load_data(db_path, 5, (0.8, 0.1, 0.1), False, True, ['SPY', 'DOW'])
-    train_titles, train_labels = to_input_label_list(train)
-    validation_titles, validation_labels = to_input_label_list(validation)
-    test_titles, test_labels = to_input_label_list(test)
-    word_counts = Counter()
-    for title in train_titles:
-        word_counts += Counter(title)
-    cuttoff = 2
-    word_counts = Vocabulary(word_counts, unk_cutoff=cuttoff)
+    real_news, [train_stock, valid_stock, test_stock] = load_data_memory_efficient(db_path, 5, (0.8, 0.1, 0.1), False, True, ["SPY", "^DJI", "NDAQ", "AAPL", "GOOG", "META"])
+    title_gen = list(map(lambda x: (x[0], nltk.word_tokenize(clean_string(' '.join(x[1])))), real_news[['date_int', 'title']].reset_index().drop(columns=['index']).groupby('date_int')['title']))
+
+    titles = []
+    for idx, tokens in title_gen:
+        if len(titles) < idx:
+            titles.extend([None for i in range(idx - len(titles))])
+        titles.append(tokens)
+
+    word_counts = Counter(np.concatenate(list(map(lambda i: titles[i], train_stock['date_int']))))
+    cutoff = 2
+    # Vocabulary(word_counts, unk_cutoff=cutoff)
+
+    # breakpoint()
+    #
+    # train_titles, train_labels = to_input_label_list(train)
+    # validation_titles, validation_labels = to_input_label_list(validation)
+    # test_titles, test_labels = to_input_label_list(test)
+    # word_counts = Counter()
+    # for title in train_titles:
+    #     word_counts += Counter(title)
+    # cuttoff = 2
+    word_counts = Vocabulary(word_counts, unk_cutoff=cutoff)
     words = sorted(word_counts, key=lambda x: (x != "<UNK>", -word_counts[x], x))
     words.remove('<UNK>')
     # used as mapping of word to id
@@ -63,17 +78,21 @@ def prepare_data():
     PAD_ID = 0
     UNK_ID = vocab["<UNK>"]
     format_titles = lambda titles: map(lambda item: list(map(lambda x: vocab[x], item)), map(vocab.lookup, titles))
-    train_data = list(zip(format_titles(train_titles), train_labels))
-    validation_data = list(zip(format_titles(validation_titles), validation_labels))
-    test_data = list(zip(format_titles(test_titles), test_labels))
+    title_data = {idx: np.bincount(np.concatenate(list(format_titles(titles[idx])), dtype=int), minlength=len(vocab)) for idx in range(len(titles)) if titles[idx] is not None}
+    # train_data = list(zip(format_titles(train_titles), train_labels))
+    # validation_data = list(zip(format_titles(validation_titles), validation_labels))
+    # test_data = list(zip(format_titles(test_titles), test_labels))
+    stock_format = lambda stock: np.ascontiguousarray(np.vstack([stock['date_int'].to_numpy(), stock['Label'].to_numpy()]).T.astype(int))
     return {
-        'train': train_data,
-        'train_dates': [x[1]['Date'] for x in train],
-        'validation': validation_data,
-        'validation_dates': [x[1]['Date'] for x in validation],
-        'test': test_data,
-        'test_dates': [x[1]['Date'] for x in test],
+        'title_data': title_data,
+        'train': stock_format(train_stock),
+        'validation': stock_format(valid_stock),
+        'test': stock_format(test_stock),
         'PAD_ID': PAD_ID,
         'UNK_ID': UNK_ID,
         'vocab': vocab
     }
+if __name__ == '__main__':
+    import os
+    os.chdir('Notebooks')
+    dat = prepare_data()
--- data.py
+++ data.py
@@ -96,9 +96,82 @@ def load_data(database_path: str, shift=5, percentage_split=(0.8, 0.1, 0.1), gap
             for i in range(len(percentage_split)):
                 lists[i].extend(data_lists[i])
         return lists
+def load_ticker_data_only(ticker, shift, percentage_split, start_date, end_int, gap, pull_from_first_section):
+    data = yfinance.download(ticker, start=str(start_date), end=str(START_DATE + datetime.timedelta(days=int(end_int) + abs(shift) + 10)))['Close'].reset_index()
+    data['Date'] = data['Date'].apply(datetime.datetime.date)
+    data['date_int'] = np.roll(list(map(int, map(date_to_int, map(str, data['Date'])))), -shift)
+    data = data[(data['date_int'] >= shift) & (data['date_int'] <= int(end_int))]
+    closing_prices = np.concatenate([data['Close'].to_numpy(), np.zeros((shift,))])
+
+    stacked = np.stack([np.roll(closing_prices, shift), closing_prices])
+    data['Label'] = (stacked[1] > stacked[0])[shift:]
+    length = len(data)
+    lengths = list(map(lambda x: round(x * length), percentage_split))[:-1]
+    start = 0
+    return_data = []
+    if gap and pull_from_first_section:
+        lengths[0] -= shift * len(lengths)
+    for subset_length in lengths:
+        if gap and not pull_from_first_section:
+            subset_length -= shift
+        return_data.append(data[start:start + subset_length])
+        start += subset_length
+        if gap:
+            start += shift
+    return_data.append(data[start:])
+    return return_data
+
+def load_data_memory_efficient(database_path: str, shift=5, percentage_split=(0.8, 0.1, 0.1), gap=False, pull_from_first_section=False, tickers=None):
+    """
+    Similar to above, but does not make copies of headline data to preserve memory
+    :param database_path: Path to headlines database
+    :param shift: The number of ticks offset the data and labels are. A shift of 5 is the same as one week as weekends
+        are clipped from the data.
+    :param percentage_split: iterable floats that sum to 1 that represent how the data is split up chronologically
+    :param gap: If true, will include a gap where the labels of one section of data overlap with the headlines of the
+        next section.
+    :param pull_from_first_section: If gap is False, this will do nothing. If True and gap is True, the first
+        section of data will be shrunk to allow the other sections to maintain their percentages. If False,
+        then each section before the last will be shortened by "shift" to accommodate the gap for
+        the proceeding section.
+    :param tickers: A list of yFinance Tickers to use. They will be added to the data in the respective data sections
+    if percentage_split is longer than length 1. Otherwise, just be appended to the data. When None, will default to ['SPY'].
+    :return: news headlines, array of length len(percentage_split) of stock data sub arrays
+    """
+    if tickers is None:
+        tickers = ["SPY"]
+    con = sqlite3.connect(database_path)
+    con.create_function("DATE_TO_INT", 1, date_to_int)
+    df = pd.read_sql("""
+    SELECT DATE_TO_INT(date) as 'date_int', name, date, title, description
+    FROM headline JOIN feed on feed.url=headline.url
+    WHERE CAST(date_int as int) >= 0 and
+     (CAST(date_int as int)) % 7 < 5 """, con)
+    con.close()
+    df['date_int'] = pd.to_numeric(df['date_int'])
+    df['date'] = pd.to_datetime(df['date']).apply(datetime.datetime.date)
+    real_news = df[(df['name'] != 'Babylon Bee (Fake News)') & (df['name'] != 'The Onion (Fake News)')].sort_values('date_int').reset_index().drop(columns=['index'])
+    end_date = max(real_news['date'])
+
+    label_end = end_date + datetime.timedelta(days=((int(date_to_int(str(end_date))) % 7) + shift) - ((int(date_to_int(str(end_date))) % 7) + shift) % 7)
+    ticker_data = [pd.DataFrame({"Date": [], "Close": []}) for i in range(len(percentage_split))]
+    for ticker in tickers:
+        split_ticker_data = load_ticker_data_only(ticker, shift, percentage_split, start_date=START_DATE,
+                                                  end_int=int(date_to_int(str(end_date))), gap=gap, pull_from_first_section=pull_from_first_section)
+        for i in range(len(percentage_split)):
+            ticker_data[i] = ticker_data[i].append(split_ticker_data[i])
+    for i in range(len(ticker_data)):
+        ticker_data[i]['date_int'] = ticker_data[i]['date_int'].astype(int)
+    return real_news, ticker_data
+
+
+
+
+
 
 
 if __name__ == '__main__':
+    with_gaps_pull_from_first_multiple_stocks_mem_efficient = load_data_memory_efficient("Data/Headlines.db", 5, (0.6, 0.1, 0.3), True, True, ['SPY', 'DOW'])
     with_gaps_pull_from_first_multiple_stocks = load_data("Data/Headlines.db", 5, (0.6, 0.1, 0.3), True, True, ['SPY', 'DOW'])
     with_gaps_pull_from_first = load_data("Data/Headlines.db", 5, (0.6, 0.1, 0.3), True, True)
     with_gaps_pull_from_preceeding = load_data("Data/Headlines.db", 5, (0.6, 0.1, 0.3), True, False)
